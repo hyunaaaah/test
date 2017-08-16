bb0b91d61ff1da401e38c506cd9f1c24124e2ba1
hb5019 <sandye@hanmail.net>
2017-08-16 15:43:06 +0900

    update carousel

--- src/js/main-carousel.js
+++ src/js/main-carousel.js
@@ -1,89 +1,42 @@
 var slides = [{
-    img: './img/mainImage1.png',
+    img: 'url(./img/mainImage1.png)',
 }, {
-    img: './img/mainImage2.jpg',
+    img: 'url(./img/mainImage2.jpg)',
 }, {
-    img: './img/mainImage3.jpg',
+    img: 'url(./img/mainImage3.jpg)',
 }, {
-    img: './img/mainImage4.jpg'
+    img: 'url(./img/mainImage4.jpg)'
 }];
 
-var carousel = $('.main-carousel-visible');
+var carousel = $('.main .main-carousel-visible');
 var currentSlideIndex = 0;
-var animating = false;
 
-function slide(direction) {
-    animating = true;
-    clearTimeout(timer);
-
+function slide(index) {
     var currentSlide = slides[currentSlideIndex];
     var nextSlide;
 
-    if (direction === 'left') {
-        nextSlide = slides[++currentSlideIndex];
-
-        if (!nextSlide) {
-            currentSlideIndex = 0;
-            nextSlide = slides[currentSlideIndex];
-        }
-    }
-    else if (direction === 'right') {
-        nextSlide = slides[--currentSlideIndex];
-
-        if (!nextSlide) {
-            currentSlideIndex = slides.length - 1;
-            nextSlide = slides[currentSlideIndex];
-        }
-    }
-
     var currentElement = carousel.find('li');
     var nextElement = $('<li></li>');
-    nextElement.css('background-image', nextSlide.img);
-    var animationLeft;
 
-    if (direction === 'left') {
-        nextElement.css('left', '100%');
-        animationLeft = '-=100%';
-    }
-    else if (direction === 'right') {
-        nextElement.css('left', '-100%');
-        animationLeft = '+=100%';
-    }
+
+
+    currentElement.css('background-image', currentSlide.img);
+    nextElement.css('background-image', slides[1].img);
+    nextElement.css('left', '1000px');
 
     carousel.append(nextElement);
 
-    carousel.find('li').animate({
-        left: animationLeft
-    }, {
-        duration: 400,
-        complete: function() {
-            currentElement.remove();
-            animating = false;
-
-            if (this === currentElement[0]) {
-                return;
-            }
-
-            timer = setTimeout(function() {
-                slide('left');
-            }, 2000);
-        }
+    $('.main-page-image-selector .first').on('click', function () {
+       currentElement.css('left', '0px');
+       nextElement.css('left', '1000px');
     });
+
+
+    $('.main-page-image-selector .second').on('click', function () {
+        currentElement.css('left', '-1000px');
+        nextElement.css('left', '0px');
+    });
+
 }
 
-$('.ht-carousel-arrow').on('click', function() {
-    if (animating) {
-        return;
-    }
-
-    if ($(this).hasClass('left')) {
-        slide('left');
-    }
-    else if ($(this).hasClass('right')) {
-        slide('right');
-    }
-});
-
-var timer = setTimeout(function() {
-    slide('left');
-}, 2000);
+slide();
