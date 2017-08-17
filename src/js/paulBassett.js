0bfd00a588511737caaa2a84ead038e50de02ea4
hb5019 <sandye@hanmail.net>
2017-08-17 17:01:47 +0900

    update barista PaulBassett

--- src/js/paulBassett.js
+++ src/js/paulBassett.js
@@ -2,6 +2,7 @@ require('bootstrap');
 require('../less/paulBassett.less');
 
 require('../js/main');
+var about = require('../js/contents/ABOUTUS');
 
 $('.paul-bassett-sub > li').on('click', function () {
     var subPageId = $(this).attr('page-id');
@@ -11,7 +12,18 @@ $('.paul-bassett-sub > li').on('click', function () {
 });
 
 function initContents() {
+    var template = require('../template/contents/ABOUTUS.hbs');
+    var template2 = require('../template/contents/baristaPaulBassett.hbs');
+
+    var html = template(about);
+
+    $('.right-contents').append(html);
+
+    var mainHtml = template2();
+
+    $('.main-contents').append(mainHtml);
 
 }
+
 
 initContents();
